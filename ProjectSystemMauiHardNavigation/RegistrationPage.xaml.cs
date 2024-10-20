d1112a56de0620b213d150b8d1921d7cbf27561c
Lenchik <aliona.nikitina.01@gmail.com>
2024-10-21 09:39:08 +1000

    готово

--- ProjectSystemMauiHardNavigation/RegistrationPage.xaml.cs
+++ ProjectSystemMauiHardNavigation/RegistrationPage.xaml.cs
@@ -1,9 +1,19 @@
+using System.Threading.Tasks;
+
 namespace ProjectSystemMauiHardNavigation;
 
 public partial class RegistrationPage : ContentPage
 {
+	public User User { get; set; }
 	public RegistrationPage()
 	{
 		InitializeComponent();
-	}
+        BindingContext = this;
+    }
+
+    private async void SaveClick(object sender, EventArgs e)
+    {
+        await DB.GetInstance().NewUser(User);
+        await Navigation.PopAsync();
+    }
 }
